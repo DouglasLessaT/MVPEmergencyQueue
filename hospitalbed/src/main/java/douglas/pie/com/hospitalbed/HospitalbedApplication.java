69b18ef65a84ea802ea4772c15494c05de65f493
Douglas <douglaslessat@gmail.com>
2024-11-24 00:24:04 -0300

    initializer project

---
+++ hospitalbed/src/main/java/douglas/pie/com/hospitalbed/HospitalbedApplication.java
@@ -0,0 +1,13 @@
+package douglas.pie.com.hospitalbed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalbedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalbedApplication.class, args);
+	}
+
+}
---
+++ hospitalbed/src/test/java/douglas/pie/com/hospitalbed/HospitalbedApplicationTests.java
@@ -0,0 +1,13 @@
+package douglas.pie.com.hospitalbed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalbedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
