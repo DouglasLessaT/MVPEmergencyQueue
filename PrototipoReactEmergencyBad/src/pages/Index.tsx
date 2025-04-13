603a3c4a16ab61bed6414d9ed79e669460094175
DOUGLAS <douglaslessat@gmail.com>
2025-04-12 21:02:02 -0300

    ajust

--- PrototipoReactEmergencyBad/src/pages/Index.tsx
+++ PrototipoReactEmergencyBad/src/pages/Index.tsx
@@ -4,8 +4,6 @@ import { useNavigate } from "react-router-dom";
 import Learning from "./Learning";
 
 const Index = () => {
-  // Now we're using Learning as our root page, so this component
-  // can either redirect to Learning or render it directly
   return <Learning />;
 };
 
